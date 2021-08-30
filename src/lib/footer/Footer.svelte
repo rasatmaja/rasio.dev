a8e17e42366d19ad41428e5e544dfdeea92ca184
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-08-30 07:38:05 +0700

    feat(repo): setup svelte project (#32)

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+    import SocialMedia from '$lib/social/Social.svelte';
+    var year = new Date().getFullYear();
+</script>
+<footer class="absolute w-full bottom-0 py-4 border-t-2 text-gray-500 text-sm mt-20">
+    <div class="md:max-w-2xl mx-4 md:mx-auto items-center flex justify-between">
+        <p>© { year } Rasio Atmaja. All rights reserved.</p>
+        <SocialMedia/>
+    </div>
+    
+</footer>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Rasio Atmaja | Home</title>
+</svelte:head>
+
+<section class="min-h-full my-auto">
+	<h2 class="text-lg font-medium tracking-wide text-secondary">HELLO THERE,</h2>
+	<h1 class="text-5xl font-black tracking-tight my-3">I'm Rasio Atmaja.</h1>
+	<h3 class="text-lg font-light text-gray-500">I'm a software engineer, Researcher</h3>
+</section>
+
