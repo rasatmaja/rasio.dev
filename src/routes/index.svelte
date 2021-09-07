ad2a61fcebc00a2b9c68f0e471c9cef46355a061
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-09-07 20:59:10 +0700

    refactor(style): add emoji (#38)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,10 +11,14 @@
 	<title>Rasio Atmaja | Home</title>
 </svelte:head>
 
-<section class="h-2/6 my-20">
-	<h2 class="text-lg font-medium tracking-wide text-secondary">HELLO THERE,</h2>
-	<h1 class="text-5xl font-black tracking-tight my-1">I'm Rasio Atmaja.</h1>
-	<h3 class="text-lg font-light text-gray-500">I'm a software engineer, Researcher</h3>
+<section class="h-2/6 my-20 flex flex-col">
+	<div class="my-5 w-max text-5xl p-5 bg-red-100 rounded-full">🤠</div>
+	<div>
+		<h2 class="text-lg font-medium tracking-wide text-secondary">HELLO THERE</h2>
+		<h1 class="text-5xl font-black tracking-tight my-2">I'm Rasio Atmaja.</h1>
+		<h3 class="text-lg font-light text-gray-500">I'm a software engineer, Researcher</h3>
+	</div>
+	
 </section>
 
 <Experience/>
