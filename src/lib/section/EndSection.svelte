27c807af900e17bcd23a67f4137afcf7273a4df6
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-09-14 19:19:39 +0700

    fix(margin): reduce margin between sections (#46)

--- src/lib/section/EndSection.svelte
+++ src/lib/section/EndSection.svelte
@@ -1,4 +1,4 @@
-<section class="my-20 py-10 px-8 rounded-2xl text-center bg-gray-100">
+<section class="my-8 md:my-20 py-10 px-8 rounded-2xl text-center bg-gray-100">
     <p class="text-7xl">🛩</p>
     <p class="text-xl font-bold my-3">That's a little story about me</p>
     <p>If you would like to discuss about your ideas 💡, programming 🤖, space 🛸 or whatever pops into your mind, you can reach me on <span><a href="https://www.linkedin.com/in/rasatmaja/" target="__blank" class="text-blue-600 font-medium">LinkedIn</a></span>, <span><a href="https://twitter.com/rasatmaja" target="__blank" class="text-blue-600 font-medium">Twitter</a></span> or send me a message at <span><a href="mailto:contact@rasio.dev" target="__blank" class="text-blue-600 font-medium">contact@rasio.dev</a></span></p>
