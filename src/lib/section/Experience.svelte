1f91d728df897a098f1b8a855b57fceb8a544b95
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-09-14 20:11:27 +0700

    refactor(data): define experience data (#47)

--- src/lib/section/Experience.svelte
+++ src/lib/section/Experience.svelte
@@ -2,6 +2,35 @@
 	export const prerender = true;
 </script>
 
+<script>
+	let experiences = [
+        {
+            "tittle":"Backend Engineer",
+            "company":"Engineering Divsion, IDN Media",
+            "dates":"2019 - Now",
+            "desc":"As backend engineer my team and i, build  and maintain the quality and stability Golang and PHP microservice to support the company’s vision to democratize information for Millennials and Gen Z. We also build workflows for Azure pipeline and Github Actions to perform automate Golang unit tests, linting, code coverage and deployment of docker images to the docker registry."
+        },
+		{
+            "tittle":"Graduated with a Computer Science Degree",
+            "company":"Computer Science Faculty, Brawijaya University",
+            "dates":"2015 - 2019",
+            "desc":"On my finals project i build an app to measure maintability index on java project. On this project i use 3 matrix, Halstead matrix, Cyclomatic Complexity and Maintaibility index itself. The value that produce from this application can help the developer to decide whether a project has a good or bad level of maintenance"
+        },
+		{
+            "tittle":"PHP Developer Intern",
+            "company":"Brawijaya Library, Brawijaya University",
+            "dates":"2018",
+            "desc":"During my internship I was challenged to design, build and deploy PHP Rest-API to meet user requirments which provides access to book information, book transactions to departments and faculties in Brawijaya University."
+        },
+		{
+            "tittle":"Lab Assitant",
+            "company":"Basis Data laboratory Computer Science Faculty",
+            "dates":"2016 - 2018",
+            "desc":"Assist lecturers and 41 students in the practical process of database systems using Microsoft SQL Server. Perform analysis and assessment of student reports in writing SQL queries."
+        },
+    ]
+</script>
+
 <section class="min-h-full my-10">
 	<div class="mb-5">
 		<h2 class="text-lg font-medium tracking-wide text-secondary">EXPERIENCE</h2>
@@ -10,32 +39,13 @@
 	</div>
 	
 	<div class="grid grid-cols-1 gap-4">
-		<div class="p-4 border rounded-lg flex flex-col space-y-1">
-			<p class="text-lg font-bold text-primary">Backend Engineer </p>
-			<p class="font-light text-gray-500">Engineering Divsion, IDN Media</p>
-			<p class="text-sm font-light text-secondary">2019 - Now</p>
-			<p class="text-gray-700">As backend engineer my team and i, build  and maintain the quality and stability Golang and PHP microservice to support the company’s vision to democratize information for Millennials and Gen Z. We also build workflows for Azure pipeline and Github Actions to perform automate Golang unit tests, linting, code coverage and deployment of docker images to the docker registry.</p>
-		</div>
-
-		<div class="p-4 border rounded-lg flex flex-col space-y-1">
-			<p class="text-lg font-bold text-primary">Graduated with a Computer Science Degree </p>
-			<p class="font-light text-gray-500">Computer Science Faculty, Brawijaya University</p>
-			<p class="text-sm font-light text-secondary">2015 - 2019</p>
-			<p class="text-gray-700">On my finals project i build an app to measure maintability index on java project. On this project i use 3 matrix, Halstead matrix, Cyclomatic Complexity and Maintaibility index itself. The value that produce from this application can help the developer to decide whether a project has a good or bad level of maintenance</p>
-		</div>
-
-		<div class="p-4 border rounded-lg flex flex-col space-y-1">
-			<p class="text-lg font-bold text-primary">PHP Developer Intern</p>
-			<p class="font-light text-gray-500">Brawijaya Library, Brawijaya University</p>
-			<p class="text-sm font-light text-secondary">2018</p>
-			<p class="text-gray-700">During my internship I was challenged to design, build and deploy PHP Rest-API to meet user requirments which provides access to book information, book transactions to departments and faculties in Brawijaya University.</p>
-		</div>
-
-		<div class="p-4 border rounded-lg flex flex-col space-y-1">
-			<p class="text-lg font-bold text-primary">Lab Assitant </p>
-			<p class="font-light text-gray-500">Basis Data laboratory Computer Science Faculty</p>
-			<p class="text-sm font-light text-secondary">2016 - 2017</p>
-			<p class="text-gray-700">Assist lecturers and 41 students in the practical process of database systems using Microsoft SQL Server. Perform analysis and assessment of student reports in writing SQL queries.</p>
-		</div>
+		{#each experiences as job}
+			<div class="p-4 border rounded-lg flex flex-col space-y-1">
+				<p class="text-lg font-bold text-primary">{job.tittle}</p>
+				<p class="font-light text-gray-500">{job.company}</p>
+				<p class="text-sm font-light text-secondary">{job.dates}</p>
+				<p class="text-gray-700">{job.desc}</p>
+			</div>
+		{/each}
 	</div>	
 </section>
